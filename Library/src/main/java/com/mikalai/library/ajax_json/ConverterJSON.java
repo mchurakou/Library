6c195803a2e1f1451b2fc0c1c674a51aaed7bea4
mchurakou <1badbug1@gmail.com>
2016-01-27 01:56:08 +0300

    - gson -> jackson

--- Library/src/main/java/com/mikalai/library/ajax_json/ConverterJSON.java
+++ Library/src/main/java/com/mikalai/library/ajax_json/ConverterJSON.java
@@ -1,22 +1,36 @@
 package com.mikalai.library.ajax_json;
 
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
 /**
  * Convert from JSON to JAVA object
  * 
  * @author Mikalai_Churakou
  */
 public class ConverterJSON {
+	private static final Logger logger = LogManager.getLogger();
 	/**
 	 * convert to Filter
 	 * 
 	 * @param str json
 	 * @return Filter
 	 */
-	public static Filter getFilter(String str){
-		Gson gson = new Gson();
-		return gson.fromJson(str, Filter.class);
+	public static Filter getFilter(String str) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		Filter obj = null;
+		try {
+			obj = mapper.readValue(str, Filter.class);
+		} catch (IOException e) {
+			logger.error("Error", e);
+		}
+
+		return obj;
 	}
 
 }
